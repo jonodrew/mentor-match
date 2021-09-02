035b47ab1994d9382d38a4c814cf83215be0c124
jonathan <jonodrew@users.noreply.github.com>
2021-09-02 20:31:19 +0100

    Update process file
    Update the process file to make it easier to tests

--- mentor-match/process.py
+++ mentor-match/process.py
@@ -1,27 +1,32 @@
 from match import Match
-from munkres import Munkres, make_cost_matrix, print_matrix
+from munkres import Munkres, make_cost_matrix, Matrix
 import sys
+from typing import Generator
+from mentee import Mentee
+from mentor import Mentor
 
-def create_mentee_list():
+
+def create_mentee_list() -> Generator[Mentee]:
     pass
 
-def create_mentor_list():
+
+def create_mentor_list() -> Generator[Mentor]:
     pass
+
 
 def create_matches() -> list[list[Match]]:
-    return [[Match(mentor, mentee) for mentor in create_mentor_list()] for mentee in create_mentee_list()]
+    return [
+        [Match(mentor, mentee) for mentor in create_mentor_list()]
+        for mentee in create_mentee_list()
+    ]
 
-def prepare_matrix():
-    return make_cost_matrix(create_matches(), lambda match: (sys.maxsize-match.quality))
+
+def prepare_matrix() -> Matrix:
+    return make_cost_matrix(
+        create_matches(), lambda match: (sys.maxsize - match.quality)
+    )
+
 
 def calculate_matches():
     algorithm = Munkres()
-    matrix = create_matches()
-    indexes = algorithm.compute(prepare_matrix())
-    print_matrix(matrix, msg='Highest profit through this matrix:')
-    total = 0
-    for row, column in indexes:
-        successful_match = matrix[row][column]
-        total += successful_match.score
-        print(f'({successful_match.mentee.name}, {successful_match.mentor.name}) -> {successful_match.score}')
-    print(f'total profit={total}')
+    return algorithm.compute(prepare_matrix())
