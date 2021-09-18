930f9f7951ea94eafd6f4337a367cc8d44233913
jonathan <jonodrew@users.noreply.github.com>
2021-09-18 18:46:41 +0100

    Add default for when on Heroku

--- app/tasks.py
+++ app/tasks.py
@@ -6,8 +6,12 @@ from celery import Celery
 celery = Celery(
     __name__,
 )
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
+celery.conf.broker_url = os.environ.get(
+    "CELERY_BROKER_URL", f"{os.environ.get('REDIS_URL')}/0"
+)
+celery.conf.result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND", f"{os.environ.get('REDIS_URL')}/0"
+)
 
 
 @celery.task(name="create_task")
