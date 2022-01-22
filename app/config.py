7e8d8b1dea9e06e0634e529841942354e505c40d
jonathan <jonodrew@users.noreply.github.com>
2022-01-22 22:40:28 +0000

    Tidy up files from rebase
    The rebase was not as neat as I'd like. Should teach me to finish things!

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config:
     CELERY_RESULT_BACKEND = os.environ.get(
         "CELERY_BROKER_URL", f"{os.environ.get('REDIS_URL')}/0")
@@ -7,8 +8,8 @@ class Config:
         "CELERY_RESULT_BACKEND", f"{os.environ.get('REDIS_URL')}/0"
     )
 
+
 class TestConfig(Config):
     CELERY_RESULT_BACKEND = "rpc://"
     CELERY_BROKER_URL = "amqp://myuser:mypassword@localhost/myvhost"
     BROKER_TRANSPORT_OPTIONS = {"max_retries": 3, "interval_start": 0, "interval_step": 0.2, "interval_max": 0.5}
-
