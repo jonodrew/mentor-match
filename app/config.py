d5221c3f434bc41770cd802002233442f6a63c22
jonathan <jonodrew@users.noreply.github.com>
2022-01-23 15:00:40 +0000

    Update config to use BROKER_URL
    Apparently Celery just asks for 'BROKER URL'

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     CELERY_RESULT_BACKEND = os.environ.get(
         "CELERY_BROKER_URL", f"{os.environ.get('REDIS_URL')}/0"
     )
-    CELERY_BROKER_URL = os.environ.get(
+    BROKER_URL = os.environ.get(
         "CELERY_RESULT_BACKEND", f"{os.environ.get('REDIS_URL')}/0"
     )
 
@@ -14,7 +14,7 @@ class TestConfig(Config):
     TESTING = True
     DEBUG = True
     CELERY_RESULT_BACKEND = "rpc://"
-    CELERY_BROKER_URL = "amqp://myuser:mypassword@localhost/myvhost"
+    BROKER_URL = "amqp://myuser:mypassword@localhost/myvhost"
     BROKER_TRANSPORT_OPTIONS = {
         "max_retries": 3,
         "interval_start": 0,
