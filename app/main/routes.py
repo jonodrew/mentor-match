be9e94bff293800de95ccbbb5b3287e3ba1b9159
John Peart <code@johnpe.art>
2022-02-14 22:11:55 +0000

    Add start page 1. Move `index.html` to `match.html` 2. Change `index.html` content to the start page 3. Add routes to `routes.py`

--- app/main/routes.py
+++ app/main/routes.py
@@ -30,6 +30,10 @@ from matching.process import create_participant_list_from_path, create_mailing_l
 def index():
     return render_template("index.html", title="Mentor matcher")
 
+@main_bp.route("/match", methods=["GET"])
+def match():
+    return render_template("match.html", title="Mentor matcher")
+
 
 @main_bp.route("/cookies")
 def cookies():
