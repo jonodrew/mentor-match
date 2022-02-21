959f5d26f5acd2715f5b51ceabecaab50355a049
jonathankerr <jonathan.kerr@education.gov.uk>
2022-02-21 16:13:59 +0000

    Add test
    This adds a test to check whether, after 15 minutes, the generated data is deleted

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,18 +1,10 @@
-import os
 import pathlib
 from unittest.mock import patch, Mock
 
 from flask import url_for, session
 
 
-def test_when_processing_uploaded_data_deleted(client, test_data_path):
-    def write_test_file(filename):
-        filepath = pathlib.Path(test_data_path, "12345", filename)
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        f = open(filepath, "w")
-        f.write("Fake data")
-        f.close()
-
+def test_when_processing_uploaded_data_deleted(client, test_data_path, write_test_file):
     for filename in ("mentors.csv", "mentees.csv"):
         write_test_file(filename)
     assert pathlib.Path(test_data_path, "12345").exists()
