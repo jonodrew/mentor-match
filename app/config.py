0f4a3f219731ca1e34ab588f51264e854b52d43a
jonathankerr <jonathan.kerr@education.gov.uk>
2022-02-23 07:13:45 +0000

    Create celery app properly

--- app/config.py
+++ app/config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config:
-    CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "BROKEN")
-    CELERY_BROKER_URL = os.environ.get("REDIS_URL", "BROKEN!!")
+    result_backend = os.environ.get("REDIS_URL", "BROKEN")
+    broker_url = os.environ.get("REDIS_URL", "BROKEN!!")
 
 
 class TestConfig(Config):
     TESTING = True
     DEBUG = True
-    BROKER_TRANSPORT_OPTIONS = {
+    broker_transport_options = {
         "max_retries": 3,
         "interval_start": 0,
         "interval_step": 0.2,
