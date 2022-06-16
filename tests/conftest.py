90b13d58fdc8f7b3e215cae537353c25b688564a
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-06-16 18:56:12 +0100

    Prepare system to accept pickle
    Pickle is a serialization library in Python that allows for complex objects to be serialized and deserialized. This will enable chaining, which in turn should allow us to run multiple attempts simultaneously

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-from typing import Mapping, Any
 
 import pytest as pytest
 import app.helpers
@@ -108,23 +107,4 @@ def celery_config():
         "broker_url": "redis://redis:6379/0",
         "result_backend": "redis://redis:6379/0",
         "accept_content": ["pickle", "json"],
-    }
-
-
-@pytest.fixture
-def celery_worker_parameters():
-    # type: () -> Mapping[str, Any]
-    """Redefine this fixture to change the init parameters of Celery workers.
-
-    This can be used e. g. to define queues the worker will consume tasks from.
-
-    The dict returned by your fixture will then be used
-    as parameters when instantiating :class:`~celery.worker.WorkController`.
-    """
-    return {
-        # For some reason this `celery.ping` is not registed IF our own worker is still
-        # running. To avoid failing tests in that case, we disable the ping check.
-        # see: https://github.com/celery/celery/issues/3642#issuecomment-369057682
-        # here is the ping task: `from celery.contrib.testing.tasks import ping`
-        "perform_ping_check": False,
     }
