67149bbc1f4847c87e5dd497608383f1929c21c6
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-06-25 10:33:41 +0100

    Improved test for notification route
    This test ensures that the correct exporter is created

--- tests/test_notify_route.py
+++ tests/test_notify_route.py
@@ -1,17 +1,33 @@
 from unittest.mock import patch
+import mock
 import pytest
 from flask import url_for
+
+
+@pytest.fixture
+def notify_client(client):
+    client.set_cookie(server_name="localhost", key="task-id", value="data")
+    yield client
+
+
+@pytest.fixture
+def patch_open_file():
+    data = mock.mock_open(read_data="first,second,third")
+    with patch(target="app.main.routes.open", new=data):
+        yield
 
 
 class TestNotifyRoute:
     @pytest.mark.unit
     @patch("app.main.routes.ExportFactory", autospec=True)
-    def test_when_route_passed_form_creates_exporter(self, patched_factory, client):
-        patch("app.main.routes.celery.group").start()
-        client.set_cookie(server_name="localhost", key="data-folder", value="test")
-        with patch.object(patched_factory, "create_exporter") as patched_creator:
-            client.post(url_for("main.notify_participants"), data={"service": "notify"})
-            patched_creator.assert_called_with("notify")
-
-    def test_notify_route_reads_all_user_data(self):
-        assert False
+    def test_when_route_passed_form_creates_exporter(
+        self, patched_factory, client, patch_open_file
+    ):
+        with patch("app.main.routes.celery.group"):
+            with patch.object(patched_factory, "create_exporter") as patched_creator:
+                client.post(
+                    url_for("main.notify_participants"), data={"service": "notify"}
+                )
+                patched_creator.assert_called_with(
+                    "notify",
+                )
