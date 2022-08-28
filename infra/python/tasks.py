7fba16c044bb6e35d428ca8fc5df5db9c5ef39df
jonathan <jonodrew@users.noreply.github.com>
2022-08-28 20:23:02 +0100

    Update task with clearer signature and instantiate objects

--- infra/python/tasks.py
+++ infra/python/tasks.py
@@ -5,14 +5,16 @@ from classes import CSMentor, CSMentee, base_rules
 
 
 def async_process_data(
-    mentors,
-    mentees,
+    mentors: list[dict],
+    mentees: list[dict],
     unmatched_bonus: int = 6,
 ) -> tuple[list[CSMentor], list[CSMentee], int]:
     all_rules = [base_rules() for _ in range(3)]
     for ruleset in all_rules:
         ruleset.append(UnmatchedBonus(unmatched_bonus))
     matched_mentors, matched_mentees = process.process_data(
-        list(mentors), list(mentees), all_rules=all_rules
+        list(CSMentor(**m) for m in mentors),
+        list(CSMentee(**m) for m in mentees),
+        all_rules=all_rules,
     )
     return matched_mentors, matched_mentees, unmatched_bonus
