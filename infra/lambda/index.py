060b693a325d6c7271a03cb398e39dbbb6920b73
jonathan <jonodrew@users.noreply.github.com>
2022-08-28 19:37:13 +0100

    Add some docstrings
    Future me will thank me!

--- infra/lambda/index.py
+++ infra/lambda/index.py
@@ -5,6 +5,12 @@ from classes import base_rules
 
 
 def async_process_data_event_handler(event: dict, context):
+    """
+    Event handler that calls the `tasks.async_process_data` function.
+    :param event: A dictionary with an event from AWS. Must have the "mentees" and "mentors" keys
+    :param context: The AWS context
+    :return:
+    """
     unmatched_bonus = event.get("unmatched bonus", 6)
     mentees = event["mentees"]
     mentors = event["mentors"]
@@ -19,10 +25,23 @@ def async_process_data_event_handler(event: dict, context):
 
 
 def find_best_result_lambda(event: dict, context):
+    """
+    Event handler to find the "best" unmatched bonus value. See `tasks.find_best_output` for implementation details
+    :param event: The triggering event. An array that should consist of
+    {"mentor": [...], "mentee": [...], "unmatched bonus": int} dicts
+    :param context: The AWS context
+    :return: A single {"mentor": [...], "mentee": [...], "unmatched bonus": int} dict
+    """
     return event
 
 
-def prepare_data_for_mapping(event: dict, context):
+def prepare_data_for_mapping(event: dict, context) -> list[dict]:
+    """
+    Create fresh copies of data, ready for handing to the mapping State defined in the infrastructure
+    :param event: The data to be matched
+    :param context: The AWS context
+    :return: A list of dicts with the format {"mentor": [...], "mentee": [...], "unmatched bonus": int}
+    """
     max_score = sum(rule.results.get(True) for rule in base_rules())
     mentees = event["mentees"]
     mentors = event["mentors"]
