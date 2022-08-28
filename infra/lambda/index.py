c989744a3a728f85b72f201d9e619fc272aa323a
jonathan <jonodrew@users.noreply.github.com>
2022-08-28 17:06:43 +0100

    Update process_data function to return something json-able

--- infra/lambda/index.py
+++ infra/lambda/index.py
@@ -5,7 +5,14 @@ def async_process_data_event_handler(event: dict, context):
     unmatched_bonus = event.get("unmatched bonus", 6)
     mentees = event["mentees"]
     mentors = event["mentors"]
-    return async_process_data(mentors, mentees, unmatched_bonus)
+    matched_mentors, matched_mentees, bonus = async_process_data(
+        mentors, mentees, unmatched_bonus
+    )
+    return {
+        "mentors": [mentor.to_dict_for_output() for mentor in matched_mentors],
+        "mentees": [mentee.to_dict_for_output() for mentee in matched_mentees],
+        "unmatched bonus": bonus,
+    }
 
 
 def find_best_result_lambda(event: dict, context):
