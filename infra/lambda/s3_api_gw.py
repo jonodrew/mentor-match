fec539f567f962b01fb3498a36107b4b876b7bd6
jonathan <jonodrew@users.noreply.github.com>
2022-08-29 16:08:53 +0100

    Change exceptions to one from client

--- infra/lambda/s3_api_gw.py
+++ infra/lambda/s3_api_gw.py
@@ -3,7 +3,6 @@ import uuid
 import os
 
 import boto3
-from aws_cdk.aws_ses_actions import S3
 
 s3_client = boto3.client("s3")
 bucket_name = os.environ.get("S3_BUCKET_NAME")
@@ -19,7 +18,7 @@ def s3_gateway_get(event, context):
     data_uuid = event["uuid"]
     try:
         return s3_client.get_object(Bucket=bucket_name, Key=data_uuid)
-    except S3.Client.exceptions as e:
+    except s3_client.exceptions as e:
         return e
 
 
@@ -36,3 +35,8 @@ def s3_gateway_delete(event, context):
     bucket = boto3.resource("s3").Bucket(bucket_name)
     data_uuid = event["uuid"]
     bucket.objects.filter(Prefix=data_uuid).delete()
+    return {
+        "statusCode": 204,
+        "headers": {"Content-Type": "application/json"},
+        "body": data_uuid,
+    }
