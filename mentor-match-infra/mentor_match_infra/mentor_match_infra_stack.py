ad9bb8c3f317519fa793b6e6eb272804eedac7f8
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-03-15 15:15:19 +0000

    Initial steps towards AWS infrastructure

---
+++ mentor-match-infra/mentor_match_infra/mentor_match_infra_stack.py
@@ -0,0 +1,30 @@
+import aws_cdk as cdk
+import aws_cdk.aws_ecs as ecs
+import aws_cdk.aws_ecs_patterns as ecsp
+from constructs import Construct
+
+
+class MentorMatchStack(cdk.Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        ecsp.ApplicationLoadBalancedFargateService(
+            self,
+            "MentorMatchWebServer",
+            task_image_options=ecsp.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry(
+                    "ghcr.io/mentor-matching-online/mentor-match/web"
+                )
+            ),
+            public_load_balancer=True,
+        )
+        ecsp.ApplicationLoadBalancedFargateService(
+            self,
+            "MentorMatchWorker",
+            task_image_options=ecsp.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry(
+                    "ghcr.io/mentor-matching-online/mentor-match/worker"
+                )
+            ),
+            public_load_balancer=True,
+        )
