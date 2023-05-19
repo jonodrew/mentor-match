020389dbccaf3e0e04e300bc432564abb771fbbd
jonathan <jonodrew@users.noreply.github.com>
2023-05-19 23:55:09 +0100

    Add beginning of infrastructure stack for mentor-match

--- mentor-match-infra/app.py
+++ mentor-match-infra/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import aws_cdk as cdk
+import os
 
-from mentor_match_infra.mentor_match_infra_stack import MentorMatchStack
+import aws_cdk as cdk
+from mentor_match_infra.infrastructure import MentorMatchStack
 
 app = cdk.App()
 MentorMatchStack(
@@ -12,7 +13,7 @@ MentorMatchStack(
     # but a single synthesized template can be deployed anywhere.
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
     # env=cdk.Environment(account='123456789012', region='us-east-1'),
---
+++ mentor-match-infra/mentor_match_infra/mentor_match_pipeline.py
@@ -0,0 +1,7 @@
+from aws_cdk import Stack
+
+
+class InfraPipeline(Stack): pass
+
+
+class ApplicationPipeline(Stack): pass
