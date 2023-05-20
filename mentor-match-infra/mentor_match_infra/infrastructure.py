83f786460cf37fb55feda06d4b227eacf7b105bc
jonathan <jonodrew@users.noreply.github.com>
2023-05-20 15:46:50 +0100

    Try a different approach to ECS

--- mentor-match-infra/mentor_match_infra/infrastructure.py
+++ mentor-match-infra/mentor_match_infra/infrastructure.py
@@ -13,29 +13,52 @@ class MentorMatchStack(cdk.Stack):
 
         cluster = ecs.Cluster(self, "EC2Cluster", vpc=vpc)
 
-        autoscaling_group = autoscaling.AutoScalingGroup(
-            self, "DefaultAutoScalingGroup",
+        cluster.add_capacity(
+            "DefaultAutoScalingGroupCapacity",
             instance_type=ec2.InstanceType("t2.nano"),
-            machine_image=ecs.EcsOptimizedImage.amazon_linux2(),
-            vpc=vpc,
+            desired_capacity=3
         )
 
-        capacity_provider = ecs.AsgCapacityProvider(self, "AsgCapacityProvider", auto_scaling_group=autoscaling_group)
-        cluster.add_asg_capacity_provider(capacity_provider)
+        ec2_task_definition = ecs.Ec2TaskDefinition(self, "TaskDef",
+                                                    network_mode=ecs.NetworkMode.BRIDGE
+                                                    )
 
-        ecs_service = ecs_patterns.NetworkLoadBalancedEc2Service(
-            self, "Ec2Service",
-            cluster=cluster,
-            memory_limit_mib=256,
-            task_image_options=ecs_patterns.NetworkLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_registry(f"ghcr.io/mentor-matching-online/mentor-match/web:{image_tag}")
-            )
-        )
+        container = ec2_task_definition.add_container("WebContainer",
+                                                      # Use the GHCR image
+                                                      image=ecs.ContainerImage.from_registry(
+                                                          f"ghcr.io/mentor-matching-online/mentor-match/web:{image_tag}"),
+                                                      memory_limit_mi_b=256
+                                                      )
+        ec2_task_definition.add_container(container)
+        service = ecs.Ec2Service(self, "Service",
+                                 cluster=cluster,
+                                 task_definition=ec2_task_definition,
+                                 desired_count=1
+                                 )
 
-        autoscaling_group.connections.allow_from_any_ipv4(port_range=ec2.Port.tcp_range(32768, 65535),
-                                                          description="allow incoming traffic from ALB")
-
-        cdk.CfnOutput(
-            self, "LoadBalancerDNS",
-            value="http://" + ecs_service.load_balancer.load_balancer_dns_name
-        )
+        # autoscaling_group = autoscaling.AutoScalingGroup(
+        #     self, "DefaultAutoScalingGroup",
+        #     instance_type=ec2.InstanceType("t2.nano"),
+        #     machine_image=ecs.EcsOptimizedImage.amazon_linux2(),
+        #     vpc=vpc,
+        # )
+        #
+        # capacity_provider = ecs.AsgCapacityProvider(self, "AsgCapacityProvider", auto_scaling_group=autoscaling_group)
+        # cluster.add_asg_capacity_provider(capacity_provider)
+        #
+        # ecs_service = ecs_patterns.NetworkLoadBalancedEc2Service(
+        #     self, "Ec2Service",
+        #     cluster=cluster,
+        #     memory_limit_mib=256,
+        #     task_image_options=ecs_patterns.NetworkLoadBalancedTaskImageOptions(
+        #         image=ecs.ContainerImage.from_registry(f"ghcr.io/mentor-matching-online/mentor-match/web:{image_tag}")
+        #     )
+        # )
+        #
+        # autoscaling_group.connections.allow_from_any_ipv4(port_range=ec2.Port.tcp_range(32768, 65535),
+        #                                                   description="allow incoming traffic from ALB")
+        #
+        # cdk.CfnOutput(
+        #     self, "LoadBalancerDNS",
+        #     value="http://" + ecs_service.load_balancer.load_balancer_dns_name
+        # )
