003dfbc544d451f163581c7b0c7412af949a930f
jonathan <jonodrew@users.noreply.github.com>
2023-05-20 11:25:21 +0100

    Add name for action

--- mentor-match-infra/mentor_match_infra/mentor_match_pipeline.py
+++ mentor-match-infra/mentor_match_infra/mentor_match_pipeline.py
@@ -23,7 +23,11 @@ class MentorMatchPipeline(Stack):
         testing_stage = MentorMatchAppStage(self, "testing", env=Environment(account="712310211354", region="eu-west-2"))
         testing_stage_deployment = pipeline.add_stage(testing_stage)
 
-        delete_test = CloudFormationDeleteStackAction(admin_permissions=True, stack_name=testing_stage_deployment.stacks.pop().stack_name)
+        delete_test = CloudFormationDeleteStackAction(
+            admin_permissions=True,
+            stack_name=testing_stage_deployment.stacks.pop().stack_name,
+            action_name="delete test stack"
+        )
 
         pipeline.add_stage(
             stage_name="Delete Test Resources",
