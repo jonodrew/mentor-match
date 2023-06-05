0e71eb30fa516199e5bc6ae544c6cfef6199c058
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-06-05 18:00:30 +0100

    Remove the need for auth
    At least while we're still disseminating the link to small groups of users and it's not permanent

--- mentor_match_web/app/main/__init__.py
+++ mentor_match_web/app/main/__init__.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, url_for, request, redirect
+from flask import Blueprint
 
 main_bp = Blueprint("main", __name__)
 
 
-@main_bp.before_request
-def check_login():
-    if not (request.cookies.get("logged-in") or request.path == url_for("auth.login")):
-        return redirect(url_for("auth.login"))
+# @main_bp.before_request
+# def check_login():
+#     if not (request.cookies.get("logged-in") or request.path == url_for("auth.login")):
+#         return redirect(url_for("auth.login"))
 
 
 from app.main import routes  # noqa: E402,F401
